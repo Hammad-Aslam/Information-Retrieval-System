4fe7687e4a9f552a30d1b47bae1eaebeea17e85d
Hammad <hammadaslam382@gmail.com>
2024-09-21 18:48:40 +0500

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="GenAI-Chat with your data",
+    version="0.0.0",
+    author="Hammad Aslam",
+    author_email="hammadaslam382@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
